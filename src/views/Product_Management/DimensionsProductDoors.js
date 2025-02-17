15ef315248640e4b59001840599c27b14c37ab81
Aayaninfotech <Aayaninfotech>
2025-02-17 11:07:25 +0530

    pop-up front-end interfaces

--- src/views/Product_Management/DimensionsProductDoors.js
+++ src/views/Product_Management/DimensionsProductDoors.js
@@ -102,84 +102,86 @@ const DimensionsProductDoors = () => {
     };
 
     return (
-        <div className="d-flex flex-column align-items-center mt-4">
-            <h1 className="mb-4 text-primary fw-bold"> Door Dimensions</h1>
-            {error && <CAlert color="danger" className="text-center">{error}</CAlert>}
-            <div className='row gy-4 gx-3  w-100'>
-                <div className='col-6 ms-auto'>
-                    <CCard className="shadow-lg border-primary rounded-3 p-3 w-100">
-                        <CCardBody>
-                            <CCardTitle className="text-center text-uppercase fw-bold text-dark">
-                                {product ? product.name : <CSpinner size="sm" />}
-                            </CCardTitle>
-                            <hr />
-                            {/* <CFormInput type="text" placeholder="Width x Height" value={frameSize} onChange={(e) => setFrameSize(e.target.value)} className="mb-3" /> */}
-                            <CFormInput
-                                type="text"
-                                placeholder="Width x Height"
-                                value={frameSize}
-                                onChange={(e) => {
-                                    const fractionMap = {
-                                        "1/2": "½",
-                                        "1/4": "¼",
-                                        "3/4": "¾",
-                                        "1/3": "⅓",
-                                        "2/3": "⅔",
-                                        "1/5": "⅕",
-                                        "2/5": "⅖",
-                                        "3/5": "⅗",
-                                        "4/5": "⅘",
-                                        "1/6": "⅙",
-                                        "5/6": "⅚",
-                                        "1/8": "⅛",
-                                        "3/8": "⅜",
-                                        "5/8": "⅝",
-                                        "7/8": "⅞"
-                                    };
+        <>
+            <div className="d-flex flex-column align-items-center mt-4">
+                <h1 className="mb-4 text-primary fw-bold"> Door Dimensions</h1>
+                {error && <CAlert color="danger" className="text-center">{error}</CAlert>}
+                <div className='row gy-4 gx-3  w-100'>
+                    <div className='col-6 ms-auto'>
+                        <CCard className="shadow-lg border-primary rounded-3 p-3 w-100">
+                            <CCardBody>
+                                <CCardTitle className="text-center text-uppercase fw-bold text-dark">
+                                    {product ? product.name : <CSpinner size="sm" />}
+                                </CCardTitle>
+                                <hr />
+                                {/* <CFormInput type="text" placeholder="Width x Height" value={frameSize} onChange={(e) => setFrameSize(e.target.value)} className="mb-3" /> */}
+                                <CFormInput
+                                    type="text"
+                                    placeholder="Width x Height"
+                                    value={frameSize}
+                                    onChange={(e) => {
+                                        const fractionMap = {
+                                            "1/2": "½",
+                                            "1/4": "¼",
+                                            "3/4": "¾",
+                                            "1/3": "⅓",
+                                            "2/3": "⅔",
+                                            "1/5": "⅕",
+                                            "2/5": "⅖",
+                                            "3/5": "⅗",
+                                            "4/5": "⅘",
+                                            "1/6": "⅙",
+                                            "5/6": "⅚",
+                                            "1/8": "⅛",
+                                            "3/8": "⅜",
+                                            "5/8": "⅝",
+                                            "7/8": "⅞"
+                                        };
 
-                                    let newValue = e.target.value;
-                                    Object.keys(fractionMap).forEach((key) => {
-                                        newValue = newValue.replace(new RegExp(key, "g"), fractionMap[key]);
-                                    });
+                                        let newValue = e.target.value;
+                                        Object.keys(fractionMap).forEach((key) => {
+                                            newValue = newValue.replace(new RegExp(key, "g"), fractionMap[key]);
+                                        });
 
-                                    setFrameSize(newValue);
-                                }}
-                                className="mb-3"
-                            />
+                                        setFrameSize(newValue);
+                                    }}
+                                    className="mb-3"
+                                />
 
-                            <CFormInput type="number" placeholder="Amount" value={amount} onChange={(e) => setamount(e.target.value)} className="mb-3" />
-                            <CButton color="primary" onClick={handleAdd} disabled={loading}>{loading ? <CSpinner size="sm" /> : '+ Add'}</CButton>
-                        </CCardBody>
-                    </CCard>
-                </div>
-                <div className='col-4 me-auto'>
-                    <div className=" w-100">
-                        {entries.length > 0 ? (
-                            entries.map((entry, index) => (
-                                <CCard key={entry._id || index} className="mb-2 shadow-sm p-2 d-flex flex-row justify-content-between align-items-center">
-                                    <div className="d-flex flex-row justify-content-between w-100 align-items-center">
-                                        <div className='d-flex flex-column gap-2 text-start fw-semibold'>
-                                            <span className='fw-bold'>Height*Width</span>
-                                            {entry.DoorWidthHeight}
+                                <CFormInput type="number" placeholder="Amount" value={amount} onChange={(e) => setamount(e.target.value)} className="mb-3" />
+                                <CButton color="primary" onClick={handleAdd} disabled={loading}>{loading ? <CSpinner size="sm" /> : '+ Add'}</CButton>
+                            </CCardBody>
+                        </CCard>
+                    </div>
+                    <div className='col-4 me-auto'>
+                        <div className=" w-100">
+                            {entries.length > 0 ? (
+                                entries.map((entry, index) => (
+                                    <CCard key={entry._id || index} className="mb-2 shadow-sm p-2 d-flex flex-row justify-content-between align-items-center">
+                                        <div className="d-flex flex-row justify-content-between w-100 align-items-center">
+                                            <div className='d-flex flex-column gap-2 text-start fw-semibold'>
+                                                <span className='fw-bold'>Height*Width</span>
+                                                {entry.DoorWidthHeight}
+                                            </div>
+                                            <div className='d-flex flex-column gap-2 text-start fw-semibold'>
+                                                <span className='fw-bold'>Amount</span>
+                                                ${entry.amount}
+                                            </div>
+                                            <CButton color="danger" size="sm" onClick={() => handleDelete(entry._id)}>
+                                                <FontAwesomeIcon icon={faTrash} />
+                                            </CButton>
                                         </div>
-                                        <div className='d-flex flex-column gap-2 text-start fw-semibold'>
-                                            <span className='fw-bold'>Amount</span>
-                                            ${entry.amount}
-                                        </div>
-                                        <CButton color="danger" size="sm" onClick={() => handleDelete(entry._id)}>
-                                            <FontAwesomeIcon icon={faTrash} />
-                                        </CButton>
-                                    </div>
 
-                                </CCard>
-                            ))
-                        ) : (
-                            <CAlert color="info" className="text-center">No dimensions found.</CAlert>
-                        )}
+                                    </CCard>
+                                ))
+                            ) : (
+                                <CAlert color="info" className="text-center">No dimensions found.</CAlert>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
