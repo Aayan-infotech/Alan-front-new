cee7bf0649a98536a1c204406317e3b583eb96f2
Imtiyaz Hussain <imtiyaz-Aayaninfotech>
2025-04-30 15:57:20 +0530

    add pyment in eway

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,21 +4,25 @@ import { CFooter } from '@coreui/react'
 const AppFooter = () => {
   return (
     <CFooter className="px-4">
-      {/* <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2024 creativeLabs.</span>
-      </div> */}
-      <div className="ms-auto">
-        <span className="me-1">Aayan infotech</span>
-        <a href="http://18.221.196.222:2040/#/dashboard" target="_blank" rel="noopener noreferrer">
-        Discount Doors and Windows
+      <div className="d-flex justify-content-between align-items-center w-100">
+        <div>
+          <span className="me-1">Developed by</span>
+          <a
+            href="https://aayaninfotech.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="me-3"
+          >
+            Aayan Infotech
+          </a>
+        </div>
+
+        <a href="https://www.discountdoorandwindow.com/" target="_blank" rel="noopener noreferrer">
+          Discount Doors and Windows
         </a>
       </div>
     </CFooter>
   )
 }
- 
 
 export default React.memo(AppFooter)
